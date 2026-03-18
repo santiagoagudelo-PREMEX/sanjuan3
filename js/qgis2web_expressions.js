e2cb29e02b6a1695bb8ea0c45d14675ed2950577
santiagoagudelo-PREMEX <santiago.agudelo@premexcorp.com>
2026-03-18 09:51:44 -0500

    Add files via upload

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,30 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_label_Rotaciones_SJ_8_eval_expression(context) {
-    // Rotacion || '\n' || AreaHA || 'ha'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (((feature.properties['Rotacion']  + '\n') + feature.properties['AreaHA'] ) + 'ha');
-    } else {
-        return (((feature['Rotacion']  + '\n') + feature['AreaHA'] ) + 'ha');
-    }
-}
-
-
-function exp_label_Rotaciones_SJ_8_eval_expression(context) {
-    // Rotacion || '\n' || AreaHA || 'ha'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (((feature.properties['Rotacion']  + '\n') + feature.properties['AreaHA'] ) + 'ha');
-    } else {
-        return (((feature['Rotacion']  + '\n') + feature['AreaHA'] ) + 'ha');
-    }
-}
